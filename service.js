e080f2c5573118b836d69a214685638fa4a5daee
ahmedSaadoun866 <ahmedsaadoun866@gmail.com>
2023-02-15 13:45:03 +0200

    change the facebook url from v 11 to 2.6

--- service.js
+++ service.js
@@ -294,7 +294,7 @@ module.exports = (app) => {
     // Send the HTTP request to the Messenger Platform
     request(
       {
-        uri: "https://graph.facebook.com/v11.0/me/messages",
+        uri: "https://graph.facebook.com/v2.6/me/messages",
         qs: { access_token: PAGE_ACCESS_TOKEN },
         method: "POST",
         json: requestBody,
